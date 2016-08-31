595da11ebbd22a32c7b015dfcef2f5853d166650
Tomasz Jakub Rup <tomasz.rup@gmail.com>
2016-08-31 22:15:33 +0200

    refactoring tests

--- src/test/java/io/github/tomi77/ems/stream/AudioImplTest.java
+++ src/test/java/io/github/tomi77/ems/stream/AudioImplTest.java
@@ -2,17 +2,34 @@ package io.github.tomi77.ems.stream;
 
 import junit.framework.TestCase;
 
-public class AudioTest extends TestCase {
+public class AudioImplTest extends TestCase {
+    private Audio audio;
 
-    public void testConstructorAndGetters() {
-        Audio audio = new AudioImpl(727893, "AAAC", "4702111241970122752", 0, 0, 2243);
+    public void setUp() throws Exception {
+        audio = new AudioImpl(727893, "AAAC", "4702111241970122752", 0, 0, 2243);
+    }
 
-        // assert statements
+    public void testGetBytesCount() throws Exception {
         assertEquals("Bytes count must be 727893", new Integer(727893), audio.getBytesCount());
+    }
+
+    public void testGetCodec() throws Exception {
         assertEquals("Codec must be AAAC", "AAAC", audio.getCodec());
+    }
+
+    public void testGetCodecNumeric() throws Exception {
         assertEquals("Codec numeric must be 4702111241970122752", new Long(Long.parseUnsignedLong("4702111241970122752")), audio.getCodecNumeric());
+    }
+
+    public void testGetDroppedBytesCount() throws Exception {
         assertEquals("Dropped bytes count must be 0", new Integer(0), audio.getDroppedBytesCount());
+    }
+
+    public void testGetDroppedPacketsCount() throws Exception {
         assertEquals("Dropped packets count must be 0", new Integer(0), audio.getDroppedPacketsCount());
+    }
+
+    public void testGetPacketsCount() throws Exception {
         assertEquals("Packets count must be 2243", new Integer(2243), audio.getPacketsCount());
     }
 }
